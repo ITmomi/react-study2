e5c1d227c56bb4cf95a4f3c5084f39accc6f4a84
yhkim3 <yhkim3@canon-bs.co.kr>
2022-02-04 15:45:13 +0900

    input / select test

---
+++ src/TodoList.jsx
@@ -0,0 +1,50 @@
+import React, { useState } from 'react';
+
+const ToDoList = () => {
+    const [text, setText] = useState('');
+    const [list, setList] = useState([]);
+    const [select, setSelect] = useState('');
+
+    const addList = () => {
+        if(list.some((item) => item === text)) {
+            alert('error');
+        } else {
+            setList([...list, text]);
+            setText('');
+        }
+    };
+
+    const del = (v) => {
+        setList(list.filter((item) => item !== v));
+    }
+
+    return (
+      <div className="main">
+          <div className="input-area">
+                <input type="text" value={text} onChange={(e) =>  setText(e.target.value)} />
+                <button onClick={addList}>Add</button>
+                <select value={select} onChange={(e) => setSelect(e.target.value)}>
+                    {list.map((v, i) => {
+                        return (
+                            <option value={v} key={i}>
+                                {v}
+                            </option>
+                        );
+                    })}
+                </select>
+          </div>
+          <div className="list">
+              {list.map((v, i) => {
+                  return (
+                      <div key={i} className="item">
+                          {v}
+                          <button onClick={() => del(v)}>delete</button>
+                      </div>
+                  );
+              })}
+          </div>
+      </div>
+    );
+};
+
+export default ToDoList;
